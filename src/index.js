6bd586a404eaec740c340226b6426550652f73fb
Ziyu Huang <ziyu.huang@ibm.com>
2017-08-13 12:13:44 -0700

    fully search functionalities

--- src/index.js
+++ src/index.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDeatil from './components/video_detail';
+import _ from 'lodash';
+const API_KEY = 'AIzaSyBRLTH01_cVHGWz7kOPieKgDgpyGteZyN4';
 
-const App = () => {
-	return <div>Hi!</div>;
-}
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {videos: [], selectedVideo: null};
+		this.videoSearch('surboards');
+	}
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, videos => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
+		});
+	}
+
+	render() {
+
+		const videoSearch = _.debounce((term) => {
+			this.videoSearch(term)
+		}, 300);
+
+		return (
+			<div>
+				<SearchBar onSearchTermChange={videoSearch}/>
+				<VideoDeatil
+					video={this.state.selectedVideo} />
+				<VideoList 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos}/>
+			</div>
+		);
+
+	};
+};
 
 
 ReactDOM.render(<App />, document.querySelector('.container'));
